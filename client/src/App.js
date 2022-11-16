ed6930c83a260ae7fb77796b74254b37f07d38db
Amelia Risner <amelia.risner0@gmail.com>
2022-11-15 20:15:51 -0800

    and we're starting from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,19 @@ import './App.css';
 import UserLogin from './UserLogin';
 
 function App() {
+
+  // fetch("https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=10")
+  // .then(function (response) {
+  //   return response.json()
+  // })
+  // .then(function (response) {
+  //   // handle ten random cards
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   // handle what went wrong
+  // });
+
   return (
    <>
    < UserLogin />
