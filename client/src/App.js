d24ab8f0a2d381550a9491d5c8ce9b093d89fe86
Amelia Risner <amelia.risner0@gmail.com>
2022-11-16 17:15:26 -0800

    singleCard component scrapped, single card function built out in main

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,32 @@
 import './App.css';
-import UserLogin from './UserLogin';
-import SingleCard from './SingleCard';
 import CardSlot from './CardSlot';
+import React, {useEffect, useState} from 'react'
 
 function App() {
 
   const randomCard = "http://localhost:3000/api/v1/spreads/random_card";
+  const [singleCard, setSingleCard] = useState(null);
 
-  // fetch("https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=10")
-  // .then(function (response) {
-  //   return response.json()
-  // })
-  // .then(function (response) {
-  //   // handle ten random cards
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   // handle what went wrong
-  // });
-
-  //randomCard, {mode: "no-cors"}
-
+  function handleClick() {
+    fetch(randomCard).then((r) => {
+        if (r.ok) {
+          r.json().then((card) => {
+            console.log(card[0])
+            setSingleCard(card[0])
+          });
+        }
+        else {
+          throw new Error("haha nope")
+        }
+      })
+    }
   
 
   return (
    <div>
     <h1 className="header">Tarot Reading~</h1>
     <div className="buttonDiv">
-    <SingleCard></SingleCard>
+    <button className='cardButton' onClick={handleClick}>Single Card</button>
     </div>
     </div>
   );
