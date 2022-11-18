10d5e6184b09ed66b386dc9cd53d0686c1353535
Amelia Risner <amelia.risner0@gmail.com>
2022-11-17 21:35:37 -0800

    ok fleshed out how to add the multiCard option, time to break some stuff and rebuild

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ function App() {
 
   const randomCard = "http://localhost:3000/api/v1/spreads/random_card";
   const [singleCard, setSingleCard] = useState("https://tarot-api.s3.amazonaws.com/images/major/5.jpg");
+  const [singleDisplay, setSingleDisplay] = useState(true);
 
-  function handleClick() {
+  function handleSingleClick() {
+    setSingleDisplay(true);
     fetch(randomCard).then((r) => {
         if (r.ok) {
           r.json().then((card) => {
@@ -21,22 +23,31 @@ function App() {
         }
       })
     }
+
+  function handlePPFClick() {
+    setSingleDisplay(false);
+    console.log("yep yep");
+  }
   
 
   return (
    <div className='wholePage'>
     <h1 className="header">Tarot Reading~</h1>
     <div className="cardSlots">
-      {/* <CardSlot/> */}
-      <CardSlot singleCard={singleCard}/>
-      {/* <CardSlot/> */}
+      {singleDisplay ? <CardSlot singleCard={singleCard}/> : null}
+      {/* <CardSlot singleCard={singleCard}/> */}
       </div>
     <div className="buttonDiv">
-    <button className='cardButton' onClick={handleClick}>Single Card</button>
-    <button>Past, Present, and Future</button>
+    <button className='cardButton' onClick={handleSingleClick}>Single Card</button>
+    <button onClick={handlePPFClick}>Past, Present, and Future</button>
     </div>
     </div>
   );
 }
 
 export default App;
+
+//ok let's pseudocode this shit --
+//if 'single card' is pressed ...
+//actually, fuck it -- let's stick a ternary in the cardSlots div that returns whether
+//it's a singleCard draw or multiCard draw (cool cool cool)
