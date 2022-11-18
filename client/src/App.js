7eaa10ba4c0fc68b56c217cd88cfa6c201dce04c
Amelia Risner <amelia.risner0@gmail.com>
2022-11-17 21:51:56 -0800

    fun w async - time to figure out what exactly that means

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,30 @@ function App() {
   const [singleCard, setSingleCard] = useState("https://tarot-api.s3.amazonaws.com/images/major/5.jpg");
   const [singleDisplay, setSingleDisplay] = useState(true);
 
+  function dealCard() {
+
+    let fullCard;
+    fetch(randomCard).then((r) => {
+      if (r.ok) {
+        r.json().then((card) => {
+          let reversedBool = Math.random() < 0.5;
+          fullCard = {...card[0], reversedCard: reversedBool}
+          console.log(fullCard)
+        });
+      }
+      else {
+        throw new Error("haha nope")
+      }
+    })
+
+    return fullCard;
+    //ok this doesn't work for a reason I strongly suspect has something to do w fetch calls being async
+    //... should probably figure out wtf that means and handle it accordingly
+    //possible workaround - store intended return value in a state function
+    //[would like to understand wtf's happening though] -- I mean it's either an async thing or a scope thing
+    //[hmmmmmm]
+  }
+
   function handleSingleClick() {
     setSingleDisplay(true);
     fetch(randomCard).then((r) => {
@@ -26,7 +50,8 @@ function App() {
 
   function handlePPFClick() {
     setSingleDisplay(false);
-    console.log("yep yep");
+    let test = dealCard();
+    console.log(test);
   }
   
 
@@ -34,7 +59,8 @@ function App() {
    <div className='wholePage'>
     <h1 className="header">Tarot Reading~</h1>
     <div className="cardSlots">
-      {singleDisplay ? <CardSlot singleCard={singleCard}/> : null}
+      {singleDisplay ? <CardSlot singleCard={singleCard}/> : 
+      null}
       {/* <CardSlot singleCard={singleCard}/> */}
       </div>
     <div className="buttonDiv">
@@ -51,3 +77,8 @@ export default App;
 //if 'single card' is pressed ...
 //actually, fuck it -- let's stick a ternary in the cardSlots div that returns whether
 //it's a singleCard draw or multiCard draw (cool cool cool)
+
+
+//motherfucking DRY principle --- ok rather than having a fuck ton of fetch calls, let's
+//rewrite that function so each handleClick can call it and get a card set
+
